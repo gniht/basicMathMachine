205705f5a498ff4ff206e2200d5876d1b92f7878
Jason Thomas <jasonthomasjat@gmail.com>
2020-09-10 16:48:19 -0700

    refactor event listeners and slight ui changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const divideButton = document.getElementById("divide");
 const resultDisplayElement = document.getElementById("resultElement");
 const putResultInForX = document.getElementById("putResultInForX");
 const putResultInForY = document.getElementById("putResultInForY");
+const controls = document.getElementById("operations");
 
 let x, y, result;
 
@@ -21,30 +22,62 @@ function assignResultToDisplayElement(){
   resultDisplayElement.textContent = `The result is: ${result}`;  
 }
 
-addButton.addEventListener('click', ()=>{
+function doTheMath(name){
+  let theMath;
+  switch(name){
+    case 'add': 
+      theMath = add(x, y);
+      break;
+    case 'subtract':
+      theMath = subtract(x, y);
+      break;
+    case 'multiply':
+      theMath = multiply(x, y);
+      break;
+    case 'divide':
+      theMath = divide(x, y);
+      break;    
+  }
+  return theMath;
+}
+
+controls.addEventListener('click', () =>{
+  
   assignXandYNumericalValues();
-  result = add(x, y);
-  assignResultToDisplayElement();    
+  if (event.target.id === 'putResultInForX'){
+    num1.valueAsNumber = result;
+  } else if (event.target.id === 'putResultInForY'){
+    num2.valueAsNumber = result;
+  } else {
+    result = doTheMath(event.target.id);
+  }
+  assignResultToDisplayElement();  
 });
-subtractButton.addEventListener('click', ()=>{
-  assignXandYNumericalValues();
-  result = subtract(x, y);
-  assignResultToDisplayElement();
-});
-multiplyButton.addEventListener('click', ()=>{
-  assignXandYNumericalValues();
-  result = multiply(x, y);
-  assignResultToDisplayElement(); 
-});
-divideButton.addEventListener('click', ()=>{
-  assignXandYNumericalValues();
-  result = divide(x, y);
-  console.log(result);
-  assignResultToDisplayElement();
-});
-putResultInForX.addEventListener('click', ()=>{
-  num1.valueAsNumber = result;    
-});
-putResultInForY.addEventListener('click', ()=>{
-  num2.valueAsNumber = result;  
-});
+
+// addButton.addEventListener('click', ()=>{
+//   assignXandYNumericalValues();
+//   result = add(x, y);
+//   assignResultToDisplayElement();    
+// });
+// subtractButton.addEventListener('click', ()=>{
+//   assignXandYNumericalValues();
+//   result = subtract(x, y);
+//   assignResultToDisplayElement();
+// });
+// multiplyButton.addEventListener('click', ()=>{
+//   assignXandYNumericalValues();
+//   result = multiply(x, y);
+//   assignResultToDisplayElement(); 
+// });
+// divideButton.addEventListener('click', ()=>{
+//   assignXandYNumericalValues();
+//   result = divide(x, y);
+//   console.log(result);
+//   assignResultToDisplayElement();
+// });
+// putResultInForX.addEventListener('click', ()=>{
+//   num1.valueAsNumber = result;    
+// });
+// putResultInForY.addEventListener('click', ()=>{
+//   num2.valueAsNumber = result;  
+// });
